aadbe394b1589b3e9119a2cdbb73581e965f5bd4
antikonst <antikonst78@gmail.com>
2022-07-26 01:17:56 +0300

    spisok

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import ExcelJS from 'exceljs';
 import * as XLSX from "xlsx";
-import moment from 'moment'
+import moment, { min } from 'moment'
 import { al } from "./amigologo";
 import { SelectRulon } from "./selectrulon";
 import { vseRulonColor } from "./vseRulonColor";
@@ -18,7 +18,12 @@ type Inputs = {
 };
 
 export default function App() {
-  const { register, getValues, watch, formState: { errors } } = useForm();
+  const { register, getValues, watch, formState: { errors }, handleSubmit } = useForm(
+    {
+      //mode: "onBlur"
+      mode: "onChange"
+    }
+  );
   //const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
   let text = 'АВЕНСИС'
   const mat = document.getElementById('mat') as HTMLSelectElement
@@ -31,17 +36,20 @@ export default function App() {
     vseRulonColor[i][0] = vseRulonColor[i][0].toUpperCase().replaceAll('BO', 'BLACK-OUT').replaceAll('_', '')
   }
   //let kn = Date.now()
-  let col = document.getElementById('col') as HTMLSelectElement
+  const col = document.getElementById('col') as HTMLSelectElement
 
-const [cl, setCl] = useState(parse('<option>300715-0225 белый</option><option>300715-1908 черный</option>'))
+  const [cl, setCl] = useState(parse('<option>300715-0225 белый</option><option>300715-1908 черный</option>'))
 
-let clo = cl
-  if (col) {
-    clo = col.options[col.selectedIndex].text
+  const [clo, setClo] = useState('300715-0225 белый')
+  //let clo = cl
+  const onClo = () => {
+    if (col) {
+      setClo(col.options[col.selectedIndex].text)
+    }
   }
 
-const onchan = () => {
-  colorRulon = ''
+  const onchan = () => {
+    colorRulon = ''
     vseRulonColor.forEach((i) => {
       if (i[0] == text) {
         let c = i[1] + ' ' + i[2]
@@ -50,15 +58,40 @@ const onchan = () => {
     })
     setCl(parse(colorRulon))
   }
- 
-  let w = watch("width")
-  let h = watch("height")
-  //let m = getValues("material")
-  //let c = getValues("color")
+
+  const [u, setU] = useState('прав')
+  const upr = document.getElementById('upr') as HTMLSelectElement
+  const onchanupr = () => {
+    if (upr) {
+      setU(upr.options[upr.selectedIndex].text)
+    }
+  }
+  /* useEffect(() => {
+    if (upr) u = upr.options[upr.selectedIndex].text
+    if (col) clo = col.options[col.selectedIndex].text
+    if (mat) text = mat.options[mat.selectedIndex].text
+  }); */
+
+  let w: number, h: number, num: number, stroka: any[], tabl: any[], tablstr: string
+  w = watch("width")
+  h = watch("height")
+  num = watch("num")
+  tabl = []
+
+  const [arrtabl, setArrtabl] = useState(tabl)
+  const [spisok, setSpisok] = useState('')
+
+  //tablstr = ''
+  //u = getValues("upr")
+  //console.log(w,h,num,u)
+  //let u = getValues("upr")
+  stroka = ['УНИ-1', text, clo, Math.ceil(w) / 1000, Math.ceil(h) / 1000, num, u, 'ст', 'бел', 'да']
+
+
+
 
   // тоже работает, но не форматирует
-  let b: string | ArrayBuffer | null = ''
-
+  /* let b: string | ArrayBuffer | null = ''
   const showFile = (e: React.FormEvent<HTMLInputElement>): void => {
     let files: FileList | null = e.currentTarget.files;
     let fil = files![0]
@@ -76,9 +109,7 @@ const onchan = () => {
     reader.onerror = function () {
       console.log(reader.error);
     };
-
-
-  }
+  } */
 
   const go = async () => {
     let date = 'Дата ' + moment().format("DD") + '.' + moment().format("MM") + '.' + moment().format("YYYY") + 'г.'
@@ -200,35 +231,75 @@ const onchan = () => {
     wsh.getCell('A1').font = { name: 'Times New Roman', size: 14, bold: true }
     wsh.mergeCells('A1:L1')
 
-    wsh.spliceRows(6, 0, ['УНИ', text, clo, Math.ceil(w / 10) / 100, Math.ceil(h / 10) / 100, 1, 'прав', 'ст', 'бел', 'да'])
-    wsh.getRow(6).alignment = { vertical: 'middle', horizontal: 'center' }
-    wsh.getRow(6).font = { name: 'Times New Roman', size: 14 }
-    for (let i = 0; i < 12; i++) {
-      let col = massA[i]
-      let c = col + 6
-      wsh.getCell(c).border = { left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
+    for (let i = 0; i < lengthSpisok; i++) {
+      let rw = 6 + i
+      wsh.spliceRows(rw, 0, arrtabl[i])
+      wsh.getRow(rw).alignment = { vertical: 'middle', horizontal: 'center' }
+      wsh.getRow(rw).font = { name: 'Times New Roman', size: 14 }
+      for (let j = 0; j < 12; j++) {
+        let col = massA[j]
+        let c = col + rw
+        wsh.getCell(c).border = { left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
+      }
+      wsh.getRow(rw).height = 58
+      wsh.getRow(rw + 1).height = 43
+
+      wsh.getCell('D' + rw).numFmt = '0.000'
+      wsh.getCell('E' + rw).numFmt = '0.000'
+
+      wsh.getCell('A' + (rw + 1)).value = '   Подпись___________                             Печать__________                                 Оплату гарантируем_____________                    С техническими особенностями ознакомлены___________'
+
     }
-    wsh.getRow(6).height = 58
-    wsh.getRow(7).height = 43
-
-    wsh.getCell('D6').numFmt = '0.00'
-    wsh.getCell('E6').numFmt = '0.00'
-
-    wsh.getCell('A7').value = '   Подпись___________                             Печать__________                                 Оплату гарантируем_____________                    С техническими особенностями ознакомлены___________'
-
     const myBase64Image = "data:image/png;base64," + al
     const imageId2 = workbook.addImage({
       base64: myBase64Image,
       extension: 'png',
     })
-    wsh.addImage(imageId2, 'J8:K11');
+    wsh.addImage(imageId2, 'J' + (8 + lengthSpisok) + ':K' + (11 + lengthSpisok));
 
     const buffer = await workbook.xlsx.writeBuffer();
     FileSaver.saveAs(new Blob([buffer]), `Заявка_УНИ_${dt}.xlsx`)
   }
 
+  let [lengthSpisok, setLengthSpisok] = useState(1)
 
   //changeRulon()
+  const addbut = document.getElementById('add')
+  const xlsxbut = document.getElementById('xlsx')
+  const tata = document.getElementById('ta')
+
+  if (lengthSpisok > 0) {
+    xlsxbut?.classList.remove('d-none')
+  } else {
+    xlsxbut?.classList.add('d-none')
+  }
+
+  if (w > 350 && w < 3300 && h > 350 && h < 3300 && num > 0 && num < 100) {
+    addbut?.classList.remove('d-none')
+  } else {
+    addbut?.classList.add('d-none')
+  }
+
+  let [strokaToStr, setStrokaToStr] = useState('')
+
+  const add = () => {
+    setStrokaToStr(strokaToStr + '\n' + stroka.join(', ').replace(/УНИ/g, lengthSpisok + '. УНИ'))
+    arrtabl.push(stroka)
+    setArrtabl(arrtabl)
+    //console.log(arrtabl)
+    setLengthSpisok(arrtabl.length + 1)
+    //let n = lengthSpisok + '. УНИ'
+    //console.log(n)
+    //setSpisok(arrtabl.join('\n').replace(/'УНИ'/g, n))
+    //tata!.innerHTML = spisok
+  }
+
+
+
+  useEffect(() => {
+    //setSpisok(arrtabl.join('\n').replace(/УНИ/g, lengthSpisok + '. УНИ'))
+    setSpisok(strokaToStr)
+  })
 
   return (
 
@@ -236,20 +307,20 @@ const onchan = () => {
     <>
       <form className="row" noValidate>
 
-<div className="row mx-auto w-75 p-0">
+        <div className="row mx-auto w-75 p-0">
 
-        <div className=" col form-floating  form-control-sm">
-          <input className="form-control" {...register("width", { required: true })} />
-          <label>ширина, мм</label>
-        </div>
-        {errors.width && <span>введите ширину</span>}
+          <div className=" col form-floating  form-control-sm">
+            <input className="form-control" {...register("width", { required: true, min: 350, max: 3300, maxLength: 4 })} />
+            <label>ширина, мм</label>
+          </div>
+          {errors.width && <span className="badge text-danger">введите ширину от 350мм до 3300мм</span>}
 
-        <div className="col form-floating  form-control-sm">
-          <input className="form-control" {...register("height", { required: true })} />
-          <label>высота, мм</label>
+          <div className="col form-floating  form-control-sm">
+            <input className="form-control" {...register("height", { required: true, min: 350, max: 3300, maxLength: 4 })} />
+            <label>высота, мм</label>
+          </div>
+          {errors.height && <span className="badge text-danger">введите высоту от 350мм до 3300мм</span>}
         </div>
-        {errors.height && <span>введите высоту</span>}
-</div>
 
 
         <div className="container-fluid w-75 form-floating  form-control-sm">
@@ -261,20 +332,53 @@ const onchan = () => {
         {errors.material && <span>материал</span>}
 
         <div className="container-fluid w-75 form-floating  form-control-sm" id="torender">
-          <select id='col' className="form-select" defaultValue='белый' {...register("color")} >
+          <select id='col' className="form-select" defaultValue='белый' {...register("color")} onClick={onClo} onChange={onClo}>
             {cl}
           </select>
           <label>цвет</label>
         </div>
-
         {errors.color && <span>цвет</span>}
- 
+
+        <div className="row mx-auto w-75 p-0">
+
+          <div className=" col form-floating  form-control-sm">
+            <input className="form-control" defaultValue={1} {...register("num", { required: true, min: 1, max: 99, maxLength: 2 })} />
+            <label>кол-во, шт</label>
+          </div>
+          {errors.num && <span>введите количество</span>}
+
+          <div className="col form-floating  form-control-sm">
+            <select id='upr' className="form-select" defaultValue="прав" {...register("upr")} onChange={onchanupr} onClick={onchanupr}>
+              <option value="прав">прав</option>
+              <option value="лев">лев</option>
+              <option value="лев">лев/прав</option>
+            </select>
+            <label>управление</label>
+          </div>
+          {errors.upr && <span>управление</span>}
+
+        </div>
+
         {/* <div className="d-flex justify-content-center mt-2">
           <input type="file" onChange={showFile}></input>
         </div> */}
+        <div className="container-fluid w-100 form-floating  form-control-sm">
+          <textarea className="form-control form-control-sm h-auto py-0 px-2" value={spisok} readOnly rows={lengthSpisok*2}></textarea>
+        </div>
       </form>
-      <div className="d-flex justify-content-center">
-        <button className="btn btn-outline-primary mt-1" onClick={go}>go</button>
+
+
+
+      <div className="row mx-auto w-75 p-0">
+
+        <div className="col d-flex justify-content-center">
+          <button id="add" className="btn btn-outline-primary mt-1 d-none" onClick={add}>++</button>
+        </div>
+
+        <div className="col d-flex justify-content-center">
+          <button id="xlsx" className="btn btn-outline-primary mt-1 d-none" onClick={go}>xlsx</button>
+        </div>
+
       </div>
     </>
 
